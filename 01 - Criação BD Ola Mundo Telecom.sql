8cfdff1666cc674a3fca953767e596c1c9f0a4e2
Guilherme Wolner <gwdm@a.recife.ifpe.edu.br>
2020-01-12 22:09:32 -0300

    MR revisado e atualizado

--- 01 - Criação BD Ola Mundo Telecom.sql
+++ 01 - Criação BD Ola Mundo Telecom.sql
@@ -1,107 +1,113 @@
-create database ola_mundo_telecom;
+DROP DATABASE ola_mundo_telecom;
 
-use ola_mundo_telecom;
+CREATE DATABASE ola_mundo_telecom;
 
-drop database ola_mundo_telecom;
+USE ola_mundo_telecom;
 
-
-
-drop table cliente;
-drop table endereco_cliente;
-drop table contato_cliente;
-drop table contrato;
-drop table endereco_instalacao;
-drop table servico;
-drop table servico_tecnico;
-drop table tecnico;
-drop table linha_telefonica;
+DROP TABLE linha_telefonica;
+DROP TABLE tecnico;
+DROP TABLE servico_x_tecnico;
+DROP TABLE servico;
+DROP TABLE endereco_servico;
+DROP TABLE contato_cliente;
+DROP TABLE endereco_cliente;
+DROP TABLE cliente;
+DROP TABLE contrato;
 
 
 
-create table cliente(
-CPF bigint,
-RG int NOT NULL,
-Nome varchar(45) NOT NULL,
-PRIMARY KEY(CPF)
+CREATE TABLE endereco_cliente(
+id_endereco_cliente INT,
+rua VARCHAR(45) NOT NULL,
+numero INT NOT NULL,
+bairro VARCHAR(45) NOT NULL,
+cidade VARCHAR(45) NOT NULL,
+PRIMARY KEY(id_endereco_cliente)
+);
+
+CREATE TABLE contato_cliente(
+id_contato_cliente INT,
+ddd INT NOT NULL,
+digitos INT NOT NULL,
+PRIMARY KEY(id_contato_cliente)
+);
+
+CREATE TABLE cliente(
+id_cliente INT,
+cpf BIGINT NOT NULL,
+rg INT NOT NULL,
+nome_cliente VARCHAR(45) NOT NULL,
+id_endereco_cliente INT,
+id_contato_cliente INT,
+PRIMARY KEY(id_cliente),
+FOREIGN KEY (id_endereco_cliente) REFERENCES endereco_cliente (id_endereco_cliente),
+FOREIGN KEY (id_contato_cliente) REFERENCES contato_cliente (id_contato_cliente)
+);
+
+CREATE TABLE endereco_servico(
+id_endereco_servico INT,
+rua VARCHAR(45) NOT NULL,
+numero INT NOT NULL,
+bairro VARCHAR(45) NOT NULL,
+cidade VARCHAR(45) NOT NULL,
+PRIMARY KEY(id_endereco_servico)
+);
+
+CREATE TABLE tecnico(
+id_tecnico INT,
+matricula INT NOT NULL,
+nome_tecnico VARCHAR(45) NOT NULL,
+cnh BIGINT(11) UNIQUE NOT NULL, 
+crea BIGINT(5) UNIQUE, 
+PRIMARY KEY(id_tecnico)
 );
 
 
-create table endereco_cliente(
-ID_End_Cliente int,
-Rua varchar(45) NOT NULL,
-Numero int NOT NULL,
-Bairro varchar(45) NOT NULL,
-Cidade varchar(45) NOT NULL,
-Cliente_CPF bigint,
-PRIMARY KEY(ID_End_Cliente),
-FOREIGN KEY (Cliente_CPF) REFERENCES cliente (CPF)
-);
-
-
-create table contato_cliente(
-ID_Contato int,
-DDD int NOT NULL,
-Digitos int NOT NULL,
-Cliente_CPF bigint,
-PRIMARY KEY(ID_Contato),
-FOREIGN KEY (Cliente_CPF) REFERENCES cliente (CPF)
-);
-
-
-create table servico(
-Protocolo int,
-Data_Emissao date NOT NULL,
-Descricao_Atividade varchar(100)NOT NULL,
-PRIMARY KEY(Protocolo)
-);
-
-
-create table endereco_servico(
-ID_End_Servico int,
-Rua varchar(45) NOT NULL,
-Numero int NOT NULL,
-Bairro varchar(45) NOT NULL,
-Cidade varchar(45) NOT NULL,
-PRIMARY KEY(ID_End_Servico)
-);
-
-
-create table contrato(
-Numero_Contrato int,
-Plano_Telefonico varchar(45) DEFAULT 'Ligação local',
-Data_Assinatura date NOT NULL,
-Cliente_CPF bigint,
-Servico_Protocolo int,
-Endereco_Servico_ID_End_Servico int,
-PRIMARY KEY(Numero_Contrato),
-FOREIGN KEY (Cliente_CPF) REFERENCES cliente (CPF),
-FOREIGN KEY (Servico_Protocolo) REFERENCES servico (Protocolo),
-FOREIGN KEY (endereco_servico_ID_End_Servico) REFERENCES endereco_servico (ID_End_Servico)
-);
-
-
-create table tecnico(
-Matricula int,
-Nome varchar(45) NOT NULL,
-Habilitacao bigint UNIQUE, 
-CREA bigint UNIQUE, 
-PRIMARY KEY(Matricula)
-);
-
-
-create table servico_tecnico(
-Servico_Protocolo int,
-Tecnico_Matricula int, 
-FOREIGN KEY (Servico_Protocolo) REFERENCES servico (Protocolo),
-FOREIGN KEY (Tecnico_Matricula) REFERENCES tecnico (Matricula)
-);
-
-
-create table linha_telefonica(
-CN int,
-Digitos int,
+CREATE TABLE linha_telefonica(
+id_linha_telefonica INT,
+cn INT NOT NULL,
+digitos INT NOT NULL,
 Tecnico_Matricula int,
-PRIMARY KEY(CN,Digitos),
-FOREIGN KEY (Tecnico_Matricula) REFERENCES tecnico (Matricula)
+PRIMARY KEY(id_linha_telefonica)
 );
+
+CREATE TABLE servico(
+id_servico INT,
+protocolo INT NOT NULL,
+data_emissao DATE NOT NULL,
+descricao_atividade TEXT(120) NOT NULL,
+id_linha_telefonica INT,
+PRIMARY KEY(id_servico),
+FOREIGN KEY (id_linha_telefonica) REFERENCES linha_telefonica (id_linha_telefonica)
+);
+
+CREATE TABLE servico_x_tecnico(
+id_tecnico INT,
+id_servico int,
+FOREIGN KEY (id_tecnico) REFERENCES tecnico (id_tecnico),
+FOREIGN KEY (id_servico) REFERENCES servico (id_servico)
+);
+
+CREATE TABLE contrato(
+id_contrato INT,
+numero_contrato INT NOT NULL,
+plano_telefonico VARCHAR(45) NOT NULL DEFAULT 'Ligação local',
+data_assinatura DATE NOT NULL,
+id_cliente INT,
+id_servico INT,
+id_endereco_servico INT,
+PRIMARY KEY(id_contrato),
+FOREIGN KEY (id_cliente) REFERENCES cliente (id_cliente),
+FOREIGN KEY (id_servico) REFERENCES servico (id_servico),
+FOREIGN KEY (id_endereco_servico) REFERENCES endereco_servico (id_endereco_servico)
+);
+
+
+
+
+
+
+
+
+
 
